59fffbcfc6ca6e8030fa861eee47918fadcf4788
johnsonlee <g.johnsonlee@gmail.com>
2022-04-08 02:20:12 +0800

    Support latest Sonatype server URL

--- src/main/kotlin/io/johnsonlee/gradle/publish/RepositoryHandlerExtensions.kt
+++ src/main/kotlin/io/johnsonlee/gradle/publish/RepositoryHandlerExtensions.kt
@@ -2,8 +2,11 @@ package io.johnsonlee.gradle.publish
 
 import org.gradle.api.Project
 import org.gradle.api.artifacts.dsl.RepositoryHandler
+import org.gradle.api.artifacts.repositories.MavenArtifactRepository
 import org.gradle.kotlin.dsl.maven
 import java.net.URI
+
+internal val SONATYPE_SERVER = URI("https://s01.oss.sonatype.org/")
 
 fun RepositoryHandler.configureNexus(project: Project, repository: String = "/content/repositories/public") {
     val NEXUS_URL: String? by project.vars
@@ -22,5 +25,18 @@ fun RepositoryHandler.configureNexus(project: Project, repository: String = "/co
 }
 
 fun RepositoryHandler.configureSonatype(project: Project) {
-    maven("https://oss.sonatype.org/service/local/staging/deploy/maven2/")
+    val OSSRH_SERVER_URL: String? by project.vars
+    val serverUrl = OSSRH_SERVER_URL?.takeIf(String::isNotBlank)?.let(::URI) ?: SONATYPE_SERVER
+    val mavenUrl = project.uri("${serverUrl.resolve("/service/local/staging/deploy/maven2/")}")
+
+    project.publishing {
+        repositories.withType(MavenArtifactRepository::class.java).find {
+            it.name == "sonatype"
+        }?.apply {
+            url = mavenUrl
+        } ?: maven {
+            name = "sonatype"
+            url = mavenUrl
+        }
+    }
 }
